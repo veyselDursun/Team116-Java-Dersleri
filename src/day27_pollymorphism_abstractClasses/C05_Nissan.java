da81a5ec860eea65636ed90ef21fe4ff69713a26
veyselDursun <veysdursun@yahoo.com>
2023-05-10 12:55:50 +0200

    C06 ve C07 Class'lar eklendi

--- src/day27_pollymorphism_abstractClasses/C05_Nissan.java
+++ src/day27_pollymorphism_abstractClasses/C05_Nissan.java
@@ -14,13 +14,17 @@ package day27_pollymorphism_abstractClasses;
             System.out.println("Nissan araclarda 20 tane gosterge vardir");
         }
         public static void main(String[] args) {
+
             C05_Nissan nissan1 = new C05_Nissan();
+
             nissan1.gosterge(); // Nissan araclarda 20 tane gosterge vardir
             nissan1.guvenlik(); // parent - Arabalarin guvenlik onlemleri olsa iyi olur
             nissan1.klima(); // parent - Arabalarin klimalari olabilir
             nissan1.motor(); // Nissan araclar renault motor kullanir
             nissan1.teker(); // Nissan araclar lassa kullanir
+
             C03_Araba nissan2 = new C05_Nissan();
+
             nissan2.gosterge(); // Nissan araclarda 20 tane gosterge vardir
             nissan2.guvenlik(); // parent - Arabalarin guvenlik onlemleri olsa iyi olur
             nissan2.klima(); // parent - Arabalarin klimalari olabilir
